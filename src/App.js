0a726a9658356040515778fc1d0ff92bc57b5f18
Andrew M Pressel <ufotofu@whistlinglemons.com>
2020-08-30 15:55:11 -0400

    baseline steps app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import qs from 'query-string';
+import Results from './results.js';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class RunnerForm extends React.Component {
+  constructor(props){
+    super(props);
+
+    const config = qs.parse(window.location.search);
+
+    this.state = {
+      wager: config.w == null ? 40 : config.w,
+      eligible: config.e == null ? '' : config.e,
+      total: config.t == null ? '' : config.t,
+      dataSet: null,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    if( event.target.id === 'wagerInput' ){
+    this.setState({wager: event.target.value});
+    }
+    if( event.target.id === 'totalInput' ){
+    this.setState({total: event.target.value});
+    }
+    if( event.target.id === 'eligibleInput' ){
+    this.setState({eligible: event.target.value});
+    }
+  }
+
+  handleSubmit(event) {
+    const dataSet = {
+     wager: parseInt(this.state.wager),
+     total: parseInt(this.state.total),
+     eligible: parseInt(this.state.eligible),
+    };
+
+    this.setState({'dataSet': dataSet});
+    event.preventDefault();
+  }
+
+  componentDidMount(){
+    if( this.state.wager != null 
+     && this.state.total != null 
+     && this.state.eligible != null ){
+    this.handleSubmit(new Event('Submit'));
+    }
+  }
+  
+  render() {
+//    alert("app.render(" + JSON.stringify(this.state.dataSet) + ")");
+    return (
+      <div className="App">
+        <div className="Form">
+          <form onSubmit={this.handleSubmit}>
+            <table border='0'>
+              <tbody>
+              <tr><td>Wager:</td><td><input id='wagerInput' type="text" value={this.state.wager} onChange={this.handleChange}/></td></tr>
+              <tr><td>Total Players:</td><td><input id='totalInput' type="text" value={this.state.total} onChange={this.handleChange}/></td></tr>
+              <tr><td>Eligible Players:</td><td><input id='eligibleInput' type="text" value={this.state.eligible} onChange={this.handleChange}/></td></tr>
+              <tr><td colSpan='2'><input type="submit" value="Compute"/></td></tr>
+              </tbody>
+            </table>
+          </form>
+        </div>
+        <hr className='divider'/ >
+        <Results dataSet={this.state.dataSet} />
+      </div>
+    );
+  }
 }
 
-export default App;
+export default RunnerForm;
